7c5cefb90ac0a1140a022c17a9c6715c0b87c171
Francescosommellaa <franc.sommella@gmail.com>
2024-09-11 18:09:55 +0200

    connected controller to home

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        // Recupera tutti i film dal database
+        $movies = Movie::all();
+
+        // Passa i film alla vista 'home'
+        return view('home', compact(['movies' => $movies]));
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,21 @@
 @endsection
 
 @section('content')
+    <div class="container my-5">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <p class="card-text">{{ $movie->nationality }}</p>
+                            <p class="card-text"><small class="text-muted">Vote: {{ $movie->vote }}</small></p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
 
 
