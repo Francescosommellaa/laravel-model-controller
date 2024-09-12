c3de54e16b1cb4b20aad7bdd3ff941895d8b32ad
Francescosommellaa <franc.sommella@gmail.com>
2024-09-12 18:51:10 +0200

    fix movies function in page controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -15,12 +15,6 @@ class PageController extends Controller
     public function movies()
     {
 
-        // SELECT * FROM `books`
-        // all() restituisce tutti i record della tabella in ordine di ID
-        // $books = Book::all();
-
-        // ottengo massimo 10  libri in ordine alfabetico in base al titolo
-        // le condizioni si mettono prima e la quesry termina con get()
         $movies = Movie::orderBy('title')->get();
 
         $title = 'Tutti i film';
